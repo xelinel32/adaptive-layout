24a791293c5934b6f724fb23be7e80cf127e5567
xelinel32 <artemsedlyar99@gmail.com>
2020-01-31 13:22:02 +0200

    create layout

---
+++ js/common.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // Match Height
+    // $('').matchHeight();
+
+
+    
+    // Animation
+    // new WOW().init();
+
+
+    // $(".smooth").on('click', function () {
+    //     var elementClick = $(this).attr("href");
+    //     var destination = $(elementClick).offset().top;
+    //     jQuery("html:not(:animated),body:not(:animated)").animate({
+    //         scrollTop: destination
+    //     }, 800);
+    //     return false;
+    // });
+
+    
+})
