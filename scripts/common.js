adcc5318d502580261afd5b7d8175ce4dbb588da
xelinel32 <artemsedlyar99@gmail.com>
2020-03-07 15:32:08 +0200

    change comment

--- scripts/common.js
+++ scripts/common.js
@@ -3,20 +3,21 @@ $(document).ready(function () {
     // Match Height
     // $('').matchHeight();
 
-
+    // $('#date').mask('99/99/9999');
+    // $('#phone').mask('(999) 999-9999');
+    // $('#tin').mask('99-9999999');
+    // $('#ssn').mask('999-99-9999');
     
     // Animation
     // new WOW().init();
 
-
     // $(".smooth").on('click', function () {
     //     var elementClick = $(this).attr("href");
     //     var destination = $(elementClick).offset().top;
-    //     jQuery("html:not(:animated),body:not(:animated)").animate({
+    //     $("html:not(:animated),body:not(:animated)").animate({
     //         scrollTop: destination
     //     }, 800);
     //     return false;
     // });
 
-    
 })
